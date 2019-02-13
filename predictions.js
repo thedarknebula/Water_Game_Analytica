4162bd3bdb3a5ea481a286815eddd8c20699e717
Ben <ThatsNoMoon.Alderaan@gmail.com>
2019-02-13 09:36:41 -0700

    Update predictions.js

--- predictions.js
+++ predictions.js
@@ -1,3 +1,13 @@
-function makePrediction(blue, red) {
-    return blue;
+function getEvents() {
+    return fetch('http://134.209.15.99/api/v1/events')
+        .then(function(response) {
+            return response.json();
+        })
+}
+
+function getOPRs(event) {
+    return fetch('http://134.209.15.99/api/v1/oprs/' + event.event_key)
+        .then(function(response) {
+            return response.json();
+        })
 }
